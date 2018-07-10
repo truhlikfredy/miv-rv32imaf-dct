ed21d4f0758fa408d4d9adac7b469e5bf44b3818
Anton Krug <anton.krug@microsemi.com>
2018-07-10 17:03:15 +0100

    Use SCH 5.3.0.31 container

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -14,5 +14,5 @@ echo "Building the $PROJECT_DIR project"
 # --rm remove the container after finished
 # --volume will mount a local folder to the docker container
 
-docker run --rm --volume $PROJECT_DIR:/project antonkrug/softconsole-5-3-slim:5400e4d bash -c "sch --drop=$HOST_UID:$HOST_GID -b=.+/Debug"
+docker run --rm --volume $PROJECT_DIR:/project antonkrug/softconsole-5-3-slim:3056bfb bash -c "sch --drop=$HOST_UID:$HOST_GID -b=.+/Debug"
 
