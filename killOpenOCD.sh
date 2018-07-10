d88f2dede642d8f7f872ba61927b568003c36a88
Anton Krug <anton.krug@microsemi.com>
2018-07-10 12:20:26 +0100

    function compatible with winder range of shells

--- killOpenOCD.sh
+++ killOpenOCD.sh
@@ -2,13 +2,13 @@
 
 echo "Will kill all existing OpenOCD and fpServer instances"
 
-kill_all_existing {
+function kill_all_existing {
     PIDS=`pidof $1`
     if [ "$PIDS" ];
     then
         # kill it even from other users, as only instance of OpenOCD can run at the same time
         # make sure you do not run these scripts in paraler 
-        
+
         sudo kill $PIDS 
     fi
 }
