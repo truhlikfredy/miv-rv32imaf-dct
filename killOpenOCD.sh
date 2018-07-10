210f2f21c8e1e21126d4460e9763630d6b197a6e
Anton Krug <anton.krug@microsemi.com>
2018-07-10 11:42:20 +0100

    kill opendocd and fpserver

---
+++ killOpenOCD.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Will kill all existing OpenOCD and fpServer instances"
+
+kill_all_existing {
+    PIDS=`pidof $1`
+    if [ "$PIDS" ];
+    then
+        # kill it even from other users, as only instance of OpenOCD can run at the same time
+        # make sure you do not run these scripts in paraler 
+        
+        sudo kill $PIDS 
+    fi
+}
+
+echo "Making sure no OpenOCD/fpServer is running"
+
+kill_all_existing openocd
+sleep 2
+
+kill_all_existing fpServer
+sleep 2
