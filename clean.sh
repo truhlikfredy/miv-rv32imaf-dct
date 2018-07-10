ba62fc2678c1aa749a4f2e406a758f777aebfbc4
Anton Krug <anton.krug@microsemi.com>
2018-07-10 18:51:58 +0100

    force clean

--- clean.sh
+++ clean.sh
@@ -2,6 +2,8 @@
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Clean the $PROJECT_DIR Debug/Release folders"
-rm -fr $PROJECT_DIR/Debug-hardfloat >/dev/null 2>&1
-rm -fr $PROJECT_DIR/Debug-softfloat >/dev/null 2>&1
-rm -fr $PROJECT_DIR/Release >/dev/null 2>&1
+rm -r -f $PROJECT_DIR/Debug-hardfloat >/dev/null 2>&1
+rm -r -f $PROJECT_DIR/Debug-softfloat >/dev/null 2>&1
+rm -r -f $PROJECT_DIR/Release >/dev/null 2>&1
+
+#rm -r -f $PROJECT_DIR/Debug-* >/dev/null 2>&1
